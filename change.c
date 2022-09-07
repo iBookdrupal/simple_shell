1ecdf3a38a900c8fb328adf43c1ec5368f9c541d
iBookdrupal <ternguh@gmail.com>
2022-09-07 03:23:42 +0100

    Modified: change.c, errors.c and Readme.md

--- change.c
+++ change.c
@@ -1,94 +1,94 @@
 
 #include "shell.h"
 /**
- * _updatepwd - change the OLDPWD env variable
- * @buf: pointer to old pwd
- * @myenv: copy of environmental variable
- * Return: the string inside PWD env variable
- */
+* _updatepwd - change the OLDPWD env variable
+* @buf: pointer to old pwd
+* @myenv: copy of environmental variable
+* Return: the string inside PWD env variable
+*/
 void _updatepwd(char *buf, char **myenv)
 {
-  char *entirepwd;
-  char str[] = "PWD=";
-  int i = 0, j = 0, cont = 0, k = 0, myenvlen = 0, buflen = 0;
+	char *entirepwd;
+	char str[] = "PWD=";
+	int i = 0, j = 0, cont = 0, k = 0, myenvlen = 0, buflen = 0;
 
-  entirepwd = str_concat(str, buf);
-  for (i = 0; myenv[i] != NULL; i++, cont = 0)
-    {
-      for (j = 0; myenv[i][j] != '\0' && j < 4; j++)
+	entirepwd = str_concat(str, buf);
+	for (i = 0; myenv[i] != NULL; i++, cont = 0)
 	{
-	  if (myenv[i][j] == str[j])
-	    {
-	      cont++;
-	    }
-	  else
-	    break;
+		for (j = 0; myenv[i][j] != '\0' && j < 4; j++)
+		{
+			if (myenv[i][j] == str[j])
+			{
+				cont++;
+			}
+			else
+				break;
+		}
+		if (cont == 4)
+			break;
 	}
-      if (cont == 4)
-	break;
-    }
-  if (cont == 4)
-    {
-      myenvlen = _strlen(myenv[i]);
-      buflen = _strlen(entirepwd);
-      if (buflen < myenvlen)
+	if (cont == 4)
 	{
-	  for (k = 0; entirepwd[k] != '\0'; k++)
-	    myenv[i][k] = entirepwd[k];
-	  for (; k < myenvlen; k++)
-	    myenv[i][k] = 0;
+		myenvlen = _strlen(myenv[i]);
+		buflen = _strlen(entirepwd);
+		if (buflen < myenvlen)
+		{
+			for (k = 0; entirepwd[k] != '\0'; k++)
+			myenv[i][k] = entirepwd[k];
+			for (; k < myenvlen; k++)
+			myenv[i][k] = 0;
+		}
+		else
+		{
+			myenv[i] = _realloc(myenv[i], myenvlen, buflen + 1);
+			for (k = 0; entirepwd[k] != '\0'; k++)
+			myenv[i][k] = entirepwd[k];
+		}
 	}
-      else
-	{
-	  myenv[i] = _realloc(myenv[i], myenvlen, buflen + 1);
-	  for (k = 0; entirepwd[k] != '\0'; k++)
-	    myenv[i][k] = entirepwd[k];
-	}
-    }
-  free(entirepwd);
+	free(entirepwd);
 }
 /**
- * _updateoldpwd - change the OLDPWD env variable
- * @buf: pointer to old pwd
- * @myenv: copy of environmental variable
- * Return: the string inside PWD env variable
- */
+* _updateoldpwd - change the OLDPWD env variable
+* @buf: pointer to old pwd
+* @myenv: copy of environmental variable
+* Return: the string inside PWD env variable
+*/
 void _updateoldpwd(char *buf, char **myenv)
 {
-  char *entirepwd;
-  char str[] = "OLDPWD=";
-  int i = 0, j = 0, cont = 0, k = 0, myenvlen = 0, buflen = 0;
+	char *entirepwd;
+	char str[] = "OLDPWD=";
+	int i = 0, j = 0, cont = 0, k = 0, myenvlen = 0, buflen = 0;
 
-  entirepwd = str_concat(str, buf);
-  for (i = 0; myenv[i] != NULL; i++)
-    {
-      for (j = 0; myenv[i][j] != '\0' && j < 7; j++)
+	entirepwd = str_concat(str, buf);
+	for (i = 0; myenv[i] != NULL; i++)
 	{
-	  if (myenv[i][j] == str[j])
-	    cont++;
-	  else
-	    break;
+		for (j = 0; myenv[i][j] != '\0' && j < 7; j++)
+		{
+			if (myenv[i][j] == str[j])
+				cont++;
+			else
+				break;
+		}
+		if (cont == 7)
+			break;
 	}
-      if (cont == 7)
-	break;
-    }
-  if (cont == 7)
-    {
-      myenvlen = _strlen(myenv[i]);
-      buflen = _strlen(entirepwd);
-      if (buflen < myenvlen)
+	if (cont == 7)
 	{
-	  for (k = 0; entirepwd[k] != '\0'; k++)
-	    myenv[i][k] = entirepwd[k];
-	  for (; k < myenvlen; k++)
-	    myenv[i][k] = 0;
+		myenvlen = _strlen(myenv[i]);
+		buflen = _strlen(entirepwd);
+		if (buflen < myenvlen)
+		{
+			for (k = 0; entirepwd[k] != '\0'; k++)
+			myenv[i][k] = entirepwd[k];
+			for (; k < myenvlen; k++)
+				myenv[i][k] = 0;
+		}
+		else
+		{
+			myenv[i] = _realloc(myenv[i], myenvlen, buflen + 1);
+			for (k = 0; entirepwd[k] != '\0'; k++)
+			myenv[i][k] = entirepwd[k];
+		}
 	}
-      else
-	{
-	  myenv[i] = _realloc(myenv[i], myenvlen, buflen + 1);
-	  for (k = 0; entirepwd[k] != '\0'; k++)
-	    myenv[i][k] = entirepwd[k];
-	}
-    }
-  free(entirepwd);
+	free(entirepwd);
 }
